3eca5923c15dcce392106902323f8cb45503489b
rbennett24 <rbennett.work@gmail.com>
2020-09-23 13:38:54 -0700

    Add files via upload

---
+++ Uspanteko_phonetic_description/Usp_cutwords.R
@@ -0,0 +1,11 @@
+cutwords<-c("yaj","tek7",                                   # Frame sentence
+            "tijb7ij","tib7ij","tb7ij","tjb7ij","tijb7iij", # Frame sentence
+            "aa","ah","ee","eh",   # Disfulencies
+            "ra","re","ree",       # Determiners and the like
+            "rere","rare","ri",    # Determiners and the like
+            "ra","laj","li","man", # Determiners and the like
+            "rii",                 # Determiners and the like
+            "tra","yaj","jun",     # Determiners and the like
+            "kita","kita7","ta",   # Negation
+            "nen"                  # WH words
+            )
